feccd8335e252e6b030c7a83df843c1ebe647ac0
b-cornett <brady.cornett30@gmail.com>
2020-05-30 21:27:10 -0600

    Updating Weather API to send current conditions email

--- Scripts/weatherAPI.py
+++ Scripts/weatherAPI.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 #Brady R. Cornett
 #Script for getting API data about the weather
-#02/15/2020
+#Uses local postal code to get data from API
 
 import yaml, requests, sys, os, json
 sys.path.append(os.path.abspath("/home/brady/workspace/python"))
 from pyMail import sendEmail
 
 if (os.path.exists('/home/brady/workspace/cfgfiles/email.yml')):
-	with open('/home/brady/workspace/cfgfiles/weatherAPI.yml') as f:
-		cfg = yaml.safe_load(f)
-		key = cfg['key']
-		postal = cfg['postal']
-	params = {'q':postal,'apikey':key}
-	url = 'http://dataservice.accuweather.com/locations/v1/search'
-	headers = {'Accept-Encoding': 'gzip,deflate'}
-	response = requests.get(url, params=params, headers=headers)
-	newKey = response.text[21:29]
+        print("Sourcing Config File")
+        with open('/home/brady/workspace/cfgfiles/weatherAPI.yml') as f:
+                cfg = yaml.safe_load(f)
+                key = cfg['key']
+                postal = cfg['postal']
+        params = {'q':postal,'apikey':key}
+        url = 'http://dataservice.accuweather.com/locations/v1/postalcodes/us/search'
+        headers = {'Accept-Encoding': 'gzip,deflate'}
+        print("Reaching out to Locations API")
+        response = requests.get(url, params=params, headers=headers)
+        data = json.loads(response.text)[0]
+        params = {'apikey':key}
+        url = 'http://dataservice.accuweather.com/currentconditions/v1/'+data['Key']
+        headers = {'Accept-Encoding': 'gzip,deflate'}
+        print("Reaching out to Current Conditions API")
+        response = requests.get(url, params=params, headers=headers)
+        currConds = json.loads(response.text)[0]
+        content = "CURRENT CONDITIONS:\n========================\nTemp: "+str(currConds['Temperature']['Imperial']['Value'])+"\nWeather: "+currConds['WeatherText']+"\nPrecipitation: "+st
+r(currConds['PrecipitationType'])+"\n========================\n\n"
+
+
+
+        sendEmail(cfg['recipients'], "Daily Weather Email", content)
 else:
-	print("Config file not available. Exiting.")
-	sys.exit(1)
+        print("Config file not available. Exiting.")
+        sys.exit(1)
