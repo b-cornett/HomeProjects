bc2e9d49fca4055269c570f4d2ebe3e7c6cfb9dc
b-cornett <brady.cornett30@gmail.com>
2020-08-14 16:54:50 -0600

    Adding updated jsx files for animated navBar

--- mern-stack-project/monster-sim/src/components/Header.jsx
+++ mern-stack-project/monster-sim/src/components/Header.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import Navbar from "./Navbar";
 
 export default class Header extends React.Component {
+  state = {
+    isMenuVisible: false,
+  };
+
+  toggleMenu = () => {
+    if (this.state.isMenuVisible == false) {
+      this.setState({ isMenuVisible: true });
+    } else {
+      this.setState({ isMenuVisible: false });
+    }
+  };
+
   render() {
     return (
       <>
         <div id="header">
-          <Navbar pages={this.props.pages} />
-          <div className="menuIcon">
+          <div className="menuIcon" onClick={this.toggleMenu}>
             <div id="bar1" className="menuBar"></div>
             <div id="bar2" className="menuBar"></div>
             <div id="bar3" className="menuBar"></div>
@@ -16,6 +27,9 @@ export default class Header extends React.Component {
           <img id="logo" src={require("./images/logo192.png")} alt="logo" />
         </div>
         <div id="lineBreak"></div>
+        <div className={`menu${this.state.isMenuVisible ? "" : "hidden"}`}>
+          <Navbar pages={this.props.pages} />
+        </div>
       </>
     );
   }
--- mern-stack-project/monster-sim/src/components/Simulation.jsx
+++ mern-stack-project/monster-sim/src/components/Simulation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class Simulation extends React.Component {
   render() {
     return (
-      <div className="simPage">
+      <div className="simPage contentPage">
         <p>SIMULATION</p>
       </div>
     );
